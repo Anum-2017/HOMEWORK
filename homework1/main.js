b7220fbae36bc37e591ee37eb4d437db9c5e6c1b
Anum Kamal <87061778+Anum-2017@users.noreply.github.com>
2024-03-25 21:14:06 +0500

    Add files via upload

---
+++ homework1/main.js
@@ -0,0 +1,7 @@
+// Unary operator
+//homework 
+var a = 5;
+var b = 2;
+var c;
+c = ++a + a++ + --b + b-- + a + b++ + b;
+console.log(c);
---
+++ homework1/main.ts
@@ -0,0 +1,8 @@
+// Unary operator
+
+//homework 
+let a: number = 5; 
+let b: number = 2;
+let c: number;
+c = ++a + a++ + --b + b-- + a + b++ + b;
+console.log(c);
