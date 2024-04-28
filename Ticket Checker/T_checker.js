4af80645ff44024ff25e5e7ed7f82a60fdf67a0d
Anum Kamal <87061778+Anum-2017@users.noreply.github.com>
2024-04-28 12:55:58 +0500

    Add files via upload

---
+++ Ticket Checker/T_checker.js
@@ -0,0 +1,16 @@
+var age = 29;
+if (age >= 0 && age <= 3) {
+    console.log("Free entry for infant");
+}
+else if (age >= 4 && age <= 12) {
+    console.log("Entry ticket for childrens 4 to 12 is RS.1000");
+}
+else if (age >= 13 && age <= 18) {
+    console.log("Entry ticket for Teenager 13 to 18 is RS.1500");
+}
+else if (age >= 19 && age <= 59) {
+    console.log("Entry ticket for Adult 19 to 59 is RS.2000");
+}
+else if (age >= 60) {
+    console.log("Entry ticket for Senior 60+ is RS.2500");
+}
